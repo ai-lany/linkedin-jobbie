95c49752c1a3117356bf86313547356205fc5f3a
Harrison Liang <liangharrison123@gmail.com>
2026-01-27 10:32:51 -0800

    add seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,86 +1,3 @@
-// const mongoose = require("mongoose");
-// const { mongoURI: db } = require('../config/keys.js');
-// const User = require('../models/User.js');
-// const Tweet = require('../models/Tweet.js');
-// const bcrypt = require('bcryptjs');
-// const { faker } = require('@faker-js/faker');
-
-// require('dotenv').config();
-
-
-
-// const NUM_SEED_USERS = 10;
-// const NUM_SEED_TWEETS = 30;
-
-// // Create users
-// const users = [];
-
-// users.push(
-//   new User ({
-//     username: 'demo-user',
-//     email: 'demo-user@appacademy.io',
-//     hashedPassword: bcrypt.hashSync('starwars', 10)
-//   })
-// )
-
-// for (let i = 1; i < NUM_SEED_USERS; i++) {
-//   const firstName = faker.name.firstName();
-//   const lastName = faker.name.lastName();
-//   users.push(
-//     new User ({
-//       username: faker.internet.userName(firstName, lastName),
-//       email: faker.internet.email(firstName, lastName),
-//       hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
-//     })
-//   )
-// }
-  
-// // Create tweets
-// const tweets = [];
-
-// for (let i = 0; i < NUM_SEED_TWEETS; i++) {
-//   tweets.push(
-//     new Tweet ({
-//       text: faker.hacker.phrase(),
-//       author: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id
-//     })
-//   )
-// }
-
-
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB successfully');
-//     insertSeeds();
-//   })
-//   .catch(err => {
-//     console.error(err.stack);
-//     process.exit(1);
-//   });
-
-
-
-//   const insertSeeds = () => {
-//     console.log("Resetting db and seeding users and tweets...");
-  
-//     User.collection.drop()
-//                    .then(() => Tweet.collection.drop())
-//                    .then(() => User.insertMany(users))
-//                    .then(() => Tweet.insertMany(tweets))
-//                    .then(() => {
-//                      console.log("Done!");
-//                      mongoose.disconnect();
-//                    })
-//                    .catch(err => {
-//                      console.error(err.stack);
-//                      process.exit(1);
-//                    });
-//   }
-
-
-
-
 require('dotenv').config();
 
 const mongoose = require("mongoose");
@@ -88,13 +5,17 @@ const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
 const Post = require('../models/Post');
 const Job = require('../models/Job');
+const Comment = require('../models/Comment');
+const Like = require('../models/Like');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
-// Create your seeds (users, posts, and jobs)
+// Create your seeds (users, posts, jobs, comments, and likes)
 const NUM_SEED_USERS = 10;
 const NUM_SEED_POSTS = 30;
 const NUM_SEED_JOBS = 20;
+const NUM_SEED_COMMENTS = 50;
+const NUM_SEED_LIKES = 80;
 
 const users = [];
 
@@ -164,16 +85,51 @@ for (let i = 0; i < NUM_SEED_JOBS; i++) {
   );
 }
 
+// Create comments on posts
+const comments = [];
+
+for (let i = 0; i < NUM_SEED_COMMENTS; i++) {
+  comments.push(
+    new Comment({
+      user: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id,
+      post: posts[Math.floor(Math.random() * NUM_SEED_POSTS)]._id,
+      text: faker.lorem.sentence()
+    })
+  );
+}
+
+// Create likes on posts and comments
+const likes = [];
+
+for (let i = 0; i < NUM_SEED_LIKES; i++) {
+  // Randomly choose to like either a post or a comment
+  const likePost = Math.random() > 0.5;
+  
+  likes.push(
+    new Like({
+      user: users[Math.floor(Math.random() * NUM_SEED_USERS)]._id,
+      likeable: likePost 
+        ? posts[Math.floor(Math.random() * NUM_SEED_POSTS)]._id
+        : comments[Math.floor(Math.random() * NUM_SEED_COMMENTS)]._id,
+      likeableType: likePost ? 'post' : 'comment'
+    })
+  );
+}
+
 // Connect to the database and insert your seeds
 const insertSeeds = () => {
-  console.log("Resetting db and seeding users, posts, and jobs...");
+  console.log("Resetting db and seeding users, posts, jobs, comments, and likes...");
 
   User.collection.drop()
     .then(() => Post.collection.drop())
     .then(() => Job.collection.drop())
+    .then(() => Comment.collection.drop())
+    .then(() => Like.collection.drop())
     .then(() => User.insertMany(users))
     .then(() => Post.insertMany(posts))
     .then(() => Job.insertMany(jobs))
+    .then(() => Comment.insertMany(comments))
+    .then(() => Like.insertMany(likes))
     .then(() => {
       console.log("Done!");
       mongoose.disconnect();
